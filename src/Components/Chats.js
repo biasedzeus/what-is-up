bd75447a03e98e051fb28c9c386bf123e04c57b5
biasedzeus <bhanu.singh1078@gmail.com>
2021-12-09 21:40:13 +0530

    minor bug removed

--- src/Components/Chats.js
+++ src/Components/Chats.js
@@ -6,24 +6,25 @@ const Chats = ({selectedUser,handleSendMessage,text,setText}) => {
     
     return (
         <div className="chat-container">
-      <div className="msgholder">
-          {selectedUser ? 
-          !selectedUser.username?<h2 className='new-conv'>Start a new conversation</h2>:
-          <h1>{selectedUser.username}</h1>:<h2 className='new-conv'>Start a new conversation</h2>
-          
-        }
-        
-     
-      </div>
-      <div className="sendmsg-btn">
-      <input
-      onChange={e => setText(e.target.value)}
-       className="msg-input" 
-       type="text"/>
-          <button
-          onClick={() =>handleSendMessage(text)}
-           className="msg-send-btn">Send</button>
-        </div>
+            <div className="msgholder">
+                {selectedUser ? 
+                !selectedUser.username?<h2 className='new-conv'>Start a new conversation</h2>:
+                <h1>{selectedUser.username}</h1>:<h2 className='new-conv'>Start a new conversation</h2>
+                
+                }
+            </div>
+
+            
+            <div className="sendmsg-btn">
+                    <input
+                    onChange={e => setText(e.target.value)}
+                    className="msg-input" 
+                    type="text"/>
+                    
+                    <button
+                    onClick={() =>handleSendMessage(text)}
+                    className="msg-send-btn">Send</button>
+            </div>
       </div>
           
     )
