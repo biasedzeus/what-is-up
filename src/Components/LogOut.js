41002e22515f0dec31bb62f1b428aa11eea15914
biasedzeus <bhanu.singh1078@gmail.com>
2021-12-09 13:22:08 +0530

    offline status

--- src/Components/LogOut.js
+++ src/Components/LogOut.js
@@ -1,11 +1,35 @@
 import React from 'react'
-import { auth } from '../firebase'
+import { auth } from '../firebase';
+import { database } from '../firebase';
+import { update,ref } from '@firebase/database';
+
 
 
 const LogOut = () => {
+    
+    
+    
+    const currentUser = auth.currentUser;
+    
+    
+
+    function updateOnlineStatus(userId) {
+        update(ref(database, "/users/" + userId), {
+          isOnline:false,
+        });
+      }
+
+  function handleLogout(){
+    auth.signOut();
+    updateOnlineStatus(currentUser.uid)
+    
+
+
+  }
+
     return (
         <div>
-            <button onClick={() => auth.signOut()}>
+            <button onClick={handleLogout}>
                 Log Out
             </button>
             
