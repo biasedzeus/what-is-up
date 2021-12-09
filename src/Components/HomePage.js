6a07d32b383c493edffe69deb9f55b957abdb850
biasedzeus <bhanu.singh1078@gmail.com>
2021-12-09 11:43:29 +0530

    homePage changed to stable

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react';
+import LogOut from './LogOut';
+import { database } from '../firebase';
+import {ref,onValue} from 'firebase/database'
+import Contacts from './Contacts';
+import ContactList from './ContactList';
 
 const HomePage = () => {
+const [messages,setMessages] = useState([]);
+   
+const dbRef= ref(database,'users/');
+useEffect(() => {
+    onValue(dbRef,(snapshot) =>{
+    const data = snapshot.val();
+    setMessages(data)
+})
+    
+}, [])
+
+console.log("messgage::",messages)
+
     return (
-        <div>
+        <div style={{backgroundColor:'yellow',width:'100vw',height:'100vh'}}>
+            <LogOut/>
             HomePage
+            <h1>Chats</h1>
+            
+            
+
         </div>
     )
 }
