cf598e479acb0649f6860e9e3764f90a3edc16c5
biasedzeus <bhanu.singh1078@gmail.com>
2021-12-09 10:39:35 +0530

    signin and setUp a new user in db completed

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React from "react";
+import { auth } from "../firebase";
+import { GoogleAuthProvider, signInWithPopup, getAuth } from "@firebase/auth";
+import { database } from "../firebase";
+import { set, ref } from "@firebase/database";
+import { useState } from "react/cjs/react.development";
 
+export const SignIn = ({ handleSignIn }) => {
+  const [userId, setUserId] = useState("");
 
+  function writeUserData(userId, name, email) {
+    set(ref(database, "/users/" + userId), {
+      username: name,
+      email: email,
+    });
+  }
 
-export const SignIn = ({handleSignIn}) => {
+  function handleSignIn() {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then(({ user }) => {
+        console.log("User::::>", user);
+        writeUserData(user.uid, user.displayName, user.email);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
-    
-
-    return (
-        <div>
-            <button
-            onClick={handleSignIn}>Sign IN</button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <button onClick={handleSignIn}>Sign IN</button>
+    </div>
+  );
+};
