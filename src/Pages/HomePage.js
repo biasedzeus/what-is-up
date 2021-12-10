f368f579ab3b5bd09ff1bd432a95310b4633e299
biasedzeus <bhanu.singh1078@gmail.com>
2021-12-10 12:56:18 +0530

    ref big solve fallabck

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -12,61 +12,23 @@ import {
   orderByChild,
   query,
   limitToLast,
-  update
+  update,
 } from "firebase/database";
 import ContactList from "../Components/ContactList";
 import Chats from "../Components/Chats";
 
 const HomePage = () => {
   const currentUser = auth.currentUser;
+
+  //STATES
   const [contactList, setContactList] = useState([]);
   const [selectedUser, SetSelectedUser] = useState([]);
   const [text, setText] = useState("");
   const [messages, setMessages] = useState([]);
-  const[lastMsgUserId,setLastMsgUserId] = useState('')
-
-  function handleUserSelect(user) {
-    console.log("slectedUser", user);
-
-    SetSelectedUser(user);
-
-    const selectedUserId = user.uid;
-    const loggedInUserId = currentUser.uid;
-
-    const primaryKey =  loggedInUserId > selectedUserId
-    ? `${loggedInUserId + selectedUserId}`
-    : `${selectedUserId + loggedInUserId}`;
-    
-    const MsgRefQuery = query(ref(database, 'messages/' + primaryKey)
-                        ,orderByChild("createdAt") ,limitToLast(50));
-
-    onValue(MsgRefQuery, (qSnapshot) =>{
-      let msges =[];
-      qSnapshot.forEach((msg) => {
-        msges.push(msg.val())
-      });
-      setMessages(msges);
-
-    })
-    const lastMsgRef =  ref(database,'lastMessage/' + primaryKey)
-     get(lastMsgRef).then(
-      (snapshot) =>{
-        setLastMsgUserId(snapshot.val().from)
-      }
-    )
-    if(lastMsgUserId !== currentUser.uid){
-       update(lastMsgRef,{
-        unread:false
-      })
-    }
-
-
-
-
-  }
-
-
-
+  const[lastMsgUserId,setLastMsgUserId] = useState('');
+  const[lastMsgFromMsges,setLastMsgFromMsges] = useState([])
+  
+  // USEFFECT
   useEffect(() => {
     const unsubscribe = onValue(
       ref(database, "/users/"),
@@ -82,8 +44,111 @@ const HomePage = () => {
   }, [currentUser]);
 
 
+  //TO SELECT USER
+  async function handleUserSelect(user) {
+                  console.log("slectedUser", user);
+
+                  SetSelectedUser(user);
+
+
+                //Current User Id and selecdUser Id
+                  const selectedUserId = user.uid;
+                  const loggedInUserId = currentUser.uid;
+                
+
+                  //Primary Key Logic
+                  const primaryKey =  loggedInUserId > selectedUserId
+                  ? `${loggedInUserId + selectedUserId}`
+                  : `${selectedUserId + loggedInUserId}`;
+                  
+
+
+                  //QUERY FOR AlL MESSAGES FROM DATABASE/MESSAGES
+                  const MsgRefQuery = query(ref(database, 'messages/' + primaryKey)
+                                      ,orderByChild("createdAt") ,limitToLast(50));
+                
+
+                  //SETTING ALL RETRIEVED MESSAGES TO A STATE : MESSAGES USING ONVALUE()
+
+                  onValue(MsgRefQuery, (qSnapshot) =>{
+                    let msges =[];
+                    qSnapshot.forEach((msg) => {
+                      msges.push(msg.val())
+                    });
+                    setMessages(msges);
+
+                  })
+
+
+                  //SETTING LAST MESSAGE REFERENCE
+                  const lastMsgRef =  ref(database,'lastMessage/' + primaryKey)
+                  
+
+                  //UPDATE LAST MSG  {SEEN:FALSE} STORED IN LASTMESSAGE/PRIMARYKEY
+
+                  //  await get(lastMsgRef, (snapshot) =>{
+                  //   // setLastMsgUserId(snapshot.val().from)
+                  //   if(snapshot.val().from !== currentUser.uid){
+                  //    update(lastMsgRef,{
+                  //      unseen:false
+                  //    })
+                  //  }
+                  // // })
+
+
+                  // await get (lastMsgRef).then((snapshot) =>{
+                  //   if(snapshot.exists()){
+                  //     if(snapshot.val().from !== currentUser.uid){
+                  //       update(lastMsgRef,
+                  //         {
+                  //           unseen:false
+                  //         })
+  
+                  //       const lastMsgFromMsgesRefQuery = query(ref(database,'messages/'+ primaryKey)  ,limitToLast(1));
+                  //       get(lastMsgFromMsgesRefQuery).then((lastSnapshot) =>{
+                  //       if(lastSnapshot.val().textMsg.localeCompare(snapshot.val().textMsg)){
+                          
+                  //         update(lastMsgFromMsgesRefQuery,{
+                  //           unseen:false
+                  //         })}
+                  //       })
+                  //     }
+                  //     }
+                  // }
+                  const lastMsgFromMsgesRefQuery = query(ref(database,'messages/'+ primaryKey)  ,limitToLast(1));
+                  await get(lastMsgRef).then((snapshot) =>{
+                      if(snapshot.exists()){
+                       if(snapshot.val().from !== currentUser.uid){
+                         update(lastMsgRef,{unseen:false});
+                         get(lastMsgFromMsgesRefQuery).then(qsnapshot =>{
+                           console.log('qsnapshot',qsnapshot)
+                             if (qsnapshot.val().textMsg.localeCompare(snapshot.val().textMsg) == 0){
+
+                              update(lastMsgFromMsgesRefQuery,{unseen:false})
+                             }
+                         }
+                         //qury endig
+                          ).catch(error =>{console.log(error)})
+                        }//compare userid ending
+                      }
+                      //if snapshot ending
+
+
+
+
+                  })
+                  //ending
+
+               
+}
+    
+
+
+  
+
 
   async function handleSendMessage() {
+    if (text === '') return;
     const loggedInUserId = currentUser.uid;
     const selectedUserId = selectedUser.uid;
     const primaryKey =
@@ -107,7 +172,7 @@ const HomePage = () => {
     textMsg: text,
     from: loggedInUserId,
     to: selectedUserId,
-    unread:true,
+    unseen:true,
    })
 
    
@@ -122,19 +187,23 @@ const HomePage = () => {
           welcome back <span>{`@${auth.currentUser.displayName}`}</span>{" "}
         </h2>
         <div>
+          {
+            console.log("msg-array",messages)
+
+          }
           <LogOut />
         </div>
       </div>
 
       <div className="flex">
-        <div class="left">
+        <div className="left">
           <ContactList
             contactList={contactList}
             handleUserSelect={handleUserSelect}
             selectedUser={selectedUser}
           />
         </div>
-        <div class="right">
+        <div className="right">
           <Chats
             selectedUser={selectedUser}
             handleSendMessage={handleSendMessage}
